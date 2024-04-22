84f55ebd1bc151ee0df76bbffcd7e83cf921106a
Nicolas Lopez <81532585+Nicolopez603@users.noreply.github.com>
2024-04-22 02:36:26 -0300

    setup: config the environment

---
+++ cypress.config.js
@@ -0,0 +1,32 @@
+const { defineConfig } = require('cypress')
+require('dotenv').config()
+
+module.exports = defineConfig({
+    e2e: {
+        supportFile: 'cypress/support/e2e.js',
+        setupNodeEvents(on, config) {
+            const env = process.env.NODE_ENV || 'production'
+            const envConfig = require(`./cypress/config/${env}`)
+            config = {
+                ...config,
+                ...envConfig,
+                env: {
+                    ...config.env,
+                    ...process.env,
+                },
+            }
+            return config
+        },
+        reporter: 'cypress-multi-reporters',
+        reporterOptions: {
+            reporterEnabled: 'mochawesome',
+            mochawesomeReporterOptions: {
+                reportDir: 'cypress/reports/mocha',
+                quiet: true,
+                overwrite: false,
+                html: false,
+                json: true,
+            },
+        },
+    },
+})
---
+++ cypress/docs/best-practice-for-commits.md
@@ -0,0 +1,15 @@
+## Best practices for commits
+
+## Commits:
+
+### To keep a project tidy when incorporating branches with new features into master/main when writing a standardized commit message, the commit message should be structured as follows:
+
+#### fix: a commit of type fix corrects a bug in the code.
+
+#### feat: a commit of type feat introduces a new feature in the code
+
+#### setup: a commit of type setup is used for environment, plugins, libraries and other configurations.
+
+#### refact: a commit of type refact is used to improve the code, both in terms of readability and maintainability.
+
+#### docs: a commit of type docs is used to add documentation and/or comments to the project.
